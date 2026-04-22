1e2afe166a4246a34d3185a0602be9768cf3f685
JoAcO3412 <joaquindevige25@gmail.com>
2026-04-21 23:50:27 -0300

    Agrego comentarios a la estructura de la clase AudioFile

--- lib/models/audio_file.dart
+++ lib/models/audio_file.dart
@@ -1,9 +1,11 @@
+//Esta clase es un modelo de datos, 
+//un molde que representa lo que es una canción dentro de la app. No tiene lógica, solo estructura.
 class AudioFile{
-  
+  //Variables declaradas: titulo, ruta y artista.
   final String title;
   final String path;
   final String artist;
-
+//Se define el constructor de la clase.
   AudioFile({required this.title, required this.path, required this.artist});
 
 
